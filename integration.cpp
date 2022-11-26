f0479ba58d78a2cb216deb3f13869269785370b1
Maser-GitHub <simone000161@gmail.com>
2022-11-26 23:59:48 +0100

    Hypersphere volume

--- integration.cpp
+++ integration.cpp
@@ -1,8 +1,16 @@
-#include<random>
-#include<iostream>
-#include<chrono>
-#include<array>
-#include<functional>
+#include <random>
+#include <iostream>
+#include <chrono>
+#include <array>
+#include <functional>
+#include <math.h>
+
+void print_vector(std::vector<double> x){
+    for(auto el : x){
+        std::cout << " " << el;
+    }
+    std::cout << std::endl;
+}
 
 // Create an hyper rectangle distribution
 //! @param boundaries in pairs [a1,b1,a2,b2,...,an,bn]
@@ -15,9 +23,35 @@ std::vector<std::uniform_real_distribution<double>> generate_hr_dist(std::vector
     return hr_dis;
 }
 
-// TODO Study between spherical coordinates vs trial-error
-std::vector<double> random_vector_hs(std::vector<double> center, double radius, std::default_random_engine &eng){
+double norm_vector(std::vector<double> v){
+    double norm = 0;
+    for(int vi:v){
+        norm += (vi*vi);
+    }
+    return norm;    
+}
 
+// TODO Study between spherical coordinates vs trial-error
+//! @param c center
+//! @param r radius
+//! @return random vector within hypersphere
+std::vector<double> random_vector_hs(std::vector<double> c, double r, std::default_random_engine &eng){
+    std::vector<double> rv;
+    std::uniform_real_distribution<double> dis(0,1);
+    int TMP_COUNT = 0;
+    do{
+        ////////////////////////rv.clear();
+        for(int i = 0; i < c.size(); i++){
+            rv.emplace_back(dis(eng));
+        }
+        if(TMP_COUNT) print_vector(rv);
+        TMP_COUNT++;
+        // TODO Check this do-while()
+    }while(norm_vector(rv) > 1.);
+    for(int i = 0; i < c.size(); i++){
+        rv[i] = rv[i] * r + c[i];
+    }
+    return rv;
 }
 
 // Returns a single random vector within an hyper rectangle
@@ -29,6 +63,21 @@ std::vector<double> random_vector_hr(std::vector<std::uniform_real_distribution<
         rv.emplace_back(hr_dis[i](eng));
     }
     return rv;
+}
+
+//! @return (n-1)!
+double gamma(double n){
+    if(n == 1.) return 1;
+    if(n == 0.5) return std::sqrt(M_PI);
+    return gamma(n-1)*(n-1);
+}
+
+double hs_hv(double r, double n){
+    // TODO choose a better threshold
+    if(n > 20){
+        return (1./std::sqrt(n*M_PI))*std::pow(2*M_PI*M_E/n,n/2.0)*std::pow(r,n);
+    }
+    return std::pow(r,n)*std::pow(M_PI, n/2.)/gamma(n/2. + 1);
 }
 
 double hr_hv(std::vector<double> &boundaries){
@@ -56,16 +105,9 @@ double MonteCarlo_integral_hr(std::function<double (std::vector<double>)> const 
     return value;
 }
 
-void print_vector(std::vector<double> &x){
-    for(auto el : x){
-        std::cout << " " << el;
-    }
-    std::cout << std::endl;
-}
-
 int main(int argc, char ** argv){
 
-    if(argc != 2){
+    /*if(argc != 2){
         std::cout << "./integration #sample" << std::endl;
         return 0;
     }
@@ -93,7 +135,17 @@ int main(int argc, char ** argv){
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);  
     std::cout << "L'integrale vale " << integral << std::endl;
-    std::cout << "In " << duration.count()  << " millisecondi" << std::endl;
+    std::cout << "In " << duration.count()  << " millisecondi" << std::endl;*/
+
+
+    std::random_device rd;
+    auto seed = rd();
+    std::default_random_engine eng(seed);
+
+    std::vector<double> center{0,0,0};
+    for(double i = 0; i < 100; i+=1){
+        random_vector_hs(center, 1, eng);
+    }
 
     return 0;
 }
