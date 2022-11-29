c8bff7f4969464cfc1e30fa7daf5c1b0cd303c2c
Maser-GitHub <simone000161@gmail.com>
2022-11-29 20:21:27 +0100

    Fully refactor and commented code

--- mc.cpp
+++ mc.cpp
@@ -5,21 +5,24 @@
 #include <functional>
 #include <math.h>
 
-
-/* Domande al prof:
-    Quanto perdo di efficenza se metto tutto quanto con V<double> al posto di std::vector<double>?
-
-
-*/
 // Everything is done with doubles for simplicity, but can be done with templates 
 
 // ================ VECTOR UTILS =======================
 
+// Vector class for when the data is conceptually a vector and not a collection
 template<typename T>
 class V{
     public: 
     std::vector<T> v;
+    V<T>() {}
+
     V<T>(std::vector<T> _v): v{_v} {}
+
+    V<T>(std::initializer_list<T> _v): v{_v} {}
+
+    V<T>(int size, T value){
+        v = std::vector<T>(size,value);
+    }
 
     V<T>& operator += (const V<T>& rhs){
         for(int i = 0; i< v.size(); i++){
@@ -34,7 +37,6 @@ class V{
         return tmp;
     }
 
-    //Adding scalar elementwise
     V<T>& operator += (T a){
         for(int i = 0; i< v.size(); i++){
             this->v[i] += a;
@@ -61,7 +63,6 @@ class V{
         return tmp;
     }
 
-    //Adding scalar elementwise
     V<T>& operator-= (T a){
         for(int i = 0; i< v.size(); i++){
             this->v[i] -= a;
@@ -75,7 +76,6 @@ class V{
         return tmp;
     }
 
-    //Per scalar product
     V<T> operator* (T a){
         V<T> tmp = *this;
         for(int i = 0; i< v.size(); i++){
@@ -96,8 +96,28 @@ class V{
         return this->v.size();
     }
 
-};
+    auto begin(){
+        return this->v.begin();
+    }
 
+    auto end(){
+        return this->v.end();
+    }
+
+    template <class... Args>
+    void emplace_back(Args&&... args){
+        this->v.emplace_back(args ...);
+    }
+
+    void clear(){
+        this->v.clear();
+    }
+
+    void pop_back(){
+        this->v.pop_back();
+    }
+
+};
 
 void print_vector(std::vector<double> x){
     for(auto el : x){
@@ -106,21 +126,40 @@ void print_vector(std::vector<double> x){
     std::cout << std::endl;
 }
 
-
-double norm_vector(std::vector<double> v){
-    double norm = 0;
-    for(double vi:v){
-        norm += (vi*vi);
+void print_vector(V<double> x){
+    for(auto el : x){
+        std::cout << " " << el;
     }
-    return norm;    
+    std::cout << std::endl;
 }
 
+//! @param v vector
+//! @return the 2-norm of a vector
 double norm_vector(V<double> v){
     double norm = 0;
     for(int i = 0; i < v.size(); i++){
         norm += v[i] * v[i];
     }
     return norm; 
+}
+
+//! @param m matrix
+//! @return determinant of matrix
+double det(V<V<double>> m){
+    double det = 1;
+    for (int c = 0; c < m.size(); c++)
+    {
+         det = det*m[c][c];
+        for (int r = c + 1; r < m.size(); r++)
+        {
+            double ratio = m[r][c] / m[c][c];
+            for (int k = c; k < m.size(); k++)
+            {
+                m[r][k] = m[r][k] - ratio * m[c][k];
+            }
+        }
+    }
+    return det;
 }
 // ============== HYPER RECTANGLE ===================
 
@@ -138,14 +177,16 @@ std::vector<std::uniform_real_distribution<double>> generate_hr_dist(std::vector
 // Returns a single random vector within an hyper rectangle
 //! @param hr_dist vector of distributions
 //! @return random vector of size n inside
-std::vector<double> random_vector_hr(std::vector<std::uniform_real_distribution<double>> &hr_dis, std::default_random_engine &eng){
-    std::vector<double> rv; //random vector
+V<double> random_vector_hr(std::vector<std::uniform_real_distribution<double>> &hr_dis, std::default_random_engine &eng){
+    V<double> rv; //random vector
     for(int i = 0; i < hr_dis.size(); i++){
         rv.emplace_back(hr_dis[i](eng));
     }
     return rv;
 }
 
+//! @param boundaries of the hyperrectangle
+//! @return hypervolume of hyperrectangle
 double hr_hv(std::vector<double> &boundaries){
     double hv = 1;
     for(int i = 0; i < boundaries.size(); i+=2)
@@ -158,9 +199,9 @@ double hr_hv(std::vector<double> &boundaries){
 //! @param c center
 //! @param r radius
 //! @return random vector on the hypersphere
-// Monte Carlo integration does not work on hyperspheres since they are volume equals to 0
-std::vector<double> random_vector_hs(std::vector<double> c, double r, std::default_random_engine &eng){
-    std::vector<double> rv;
+// Monte Carlo integration does not work on hyperspheres since they have volume equals to 0
+V<double> random_vector_hs(V<double> c, double r, std::default_random_engine &eng){
+    V<double> rv;
     std::uniform_real_distribution<double> dis(-10,10); //-10 to 10 in order to avoid floating point errors while normalizing dividing by a small value
     for(int i = 0; i < c.size(); i++){
         rv.emplace_back(dis(eng));
@@ -176,9 +217,9 @@ std::vector<double> random_vector_hs(std::vector<double> c, double r, std::defau
 //! @param c center
 //! @param r radius
 //! @return random vector within hyperball
-std::vector<double> random_vector_hb(std::vector<double> c, double r, std::default_random_engine &eng){
-    std::vector<double> rv;
-    std::vector<double> origin(c.size()+2, 0.);
+V<double> random_vector_hb(V<double> c, double r, std::default_random_engine &eng){
+    V<double> rv;
+    V<double> origin(c.size()+2, 0.);
     rv = random_vector_hs(origin,1,eng);
     rv.pop_back();
     rv.pop_back();
@@ -192,8 +233,8 @@ std::vector<double> random_vector_hb(std::vector<double> c, double r, std::defau
 //! @param c center
 //! @param r radius
 //! @return random vector within hyperball
-std::vector<double> random_vector_hb_inefficient(std::vector<double> c, double r, std::default_random_engine &eng){
-    std::vector<double> rv;
+V<double> random_vector_hb_inefficient(V<double> c, double r, std::default_random_engine &eng){
+    V<double> rv;
     std::uniform_real_distribution<double> dis(-1,1);
     do{
         rv.clear();
@@ -208,6 +249,7 @@ std::vector<double> random_vector_hb_inefficient(std::vector<double> c, double r
 }
 
 
+//! @param n
 //! @return (n-1)!
 double gamma(double n){
     if(n == 1.) return 1;
@@ -231,25 +273,8 @@ double hb_hv(double r, double n){
 
 
 // https://mathworld.wolfram.com/TrianglePointPicking.html
-// quindi scelgo poi se tagliare o ribaltare (probabilmente la seconda)
-
-double det(V<V<double>> m){
-    double det = 1;
-    for (int c = 0; c < m.size(); c++)
-    {
-         det = det*m[c][c];
-        for (int r = c + 1; r < m.size(); r++)
-        {
-            double ratio = m[r][c] / m[c][c];
-            for (int k = c; k < m.size(); k++)
-            {
-                m[r][k] = m[r][k] - ratio * m[c][k];
-            }
-        }
-    }
-    return det;
-}
-
+//! @param vs collection of vertices
+//! @return hypervolume of simplex
 double ht_hv(std::vector<V<double>> vs){
     V<V<double>> m{vs};
     V<double> v0 = m[0];
@@ -258,7 +283,11 @@ double ht_hv(std::vector<V<double>> vs){
     return 1./gamma(v0.size() + 1) * det(m); //https://en.wikipedia.org/wiki/Simplex#Volume
 }
 
-std::vector<double> random_vector_ht(std::vector<V<double>> vs, std::default_random_engine &eng){
+
+//! @param vs collection of vertices
+//! @param eng engine for random numbers
+//! @return hypervolume of simplex
+V<double> random_vector_ht(std::vector<V<double>> vs, std::default_random_engine &eng){
     int i,j;
     //Inefficent check
     if(vs.size() != (vs[0].size() + 1)){
@@ -278,7 +307,7 @@ std::vector<double> random_vector_ht(std::vector<V<double>> vs, std::default_ran
     for(auto a:as){
         sum += a;
     }
-    // Il the conic combination exceed the convex combination
+    // If the conic combination exceed the convex combination
     if(sum > 1){
         // Reflection by the plane a1+a2+...+an=1
         double t = 1;
@@ -300,7 +329,25 @@ std::vector<double> random_vector_ht(std::vector<V<double>> vs, std::default_ran
 // ============================ HYPER POLYTOPES ======================
 
 // TODO Implement division of polytopes in simplexes
-// insight: Prendo il politopo, lo divido in hyper-triangoli e ne scelgo uno a caso pensandolo in base al volume.
+// insight: By bycentric subdivision divide the polytopes in simpleces and then pick an element from them
+
+/*
+// It takes the vertices of a convex polytope and returns a vector of simplex recursively
+std::vector<std::vector<V<double>>> simplexes(std::vector<V<double>> vs){
+    std::vector<std::vector<V<double>>> simplexes;
+    if(vs.size() == 0){
+        std::cout << "Error, no vector passed" << std::endl;
+        return;
+    }
+    int dim = vs[0].size()
+    if(vs.size() == 1){
+
+    }
+    if(vs.size() == 2){
+ 
+    }
+};
+*/
 
 
 // ======================== MONTECARLO INTEGRALS ===================
@@ -308,9 +355,8 @@ std::vector<double> random_vector_ht(std::vector<V<double>> vs, std::default_ran
 //! @param f is the function to integrate
 //! @param boundaries of the hyper rectangle
 //! @param sample_size iterations
-// TODO togli e metti le reference per vedere l'efficenza, non dovrebbe cambiare molto perche' sono vettori n-dimensionali
-// TODO Non e' bello che eng passi fra cosi' tante funzioni
-double MonteCarlo_integral_hr(std::function<double (std::vector<double>)> const &f, std::vector<double> &boundaries,
+//! @param eng engine for random numbers
+double MonteCarlo_integral_hr(std::function<double (V<double>)> const &f, std::vector<double> &boundaries,
                              int sample_size, std::default_random_engine &eng){
     std::vector<std::uniform_real_distribution<double>> hr_dis = generate_hr_dist(boundaries);
     double area = hr_hv(boundaries);
@@ -322,7 +368,12 @@ double MonteCarlo_integral_hr(std::function<double (std::vector<double>)> const 
     return value;
 }
 
-double MonteCarlo_integral_hb(std::function<double (std::vector<double>)> const &f, std::vector<double> &c, double r,
+//! @param f is the function to integrate
+//! @param c center of the ball
+//! @param r radius of the ball
+//! @param sample_size iterations
+//! @param eng engine for random numbers
+double MonteCarlo_integral_hb(std::function<double (V<double>)> const &f, V<double> &c, double r,
                              int sample_size, std::default_random_engine &eng){
     double area = hb_hv(r, c.size());
     double value;
@@ -346,8 +397,8 @@ int main(int argc, char ** argv){
     auto seed = rd();
     std::default_random_engine eng(seed);
 
-    // Funzione: x1*x2*...*xn
-    auto produttoria = [](std::vector<double> x){
+    // Function: x1*x2*...*xn
+    auto f = [](V<double> x){
         double r = 1;
         for(int i = 0; i < x.size(); i++){
             r*=x[i];
@@ -355,23 +406,15 @@ int main(int argc, char ** argv){
         return r;
     };
 
-    std::vector<double> center{3, 4, 5};
+    V<double> center{3, 4, 5};
     double radius = 2;
     
-    /*auto start = std::chrono::high_resolution_clock::now();
-    double integral = MonteCarlo_integral_hb(produttoria, center, radius, sample_size, eng);
+    auto start = std::chrono::high_resolution_clock::now();
+    double integral = MonteCarlo_integral_hb(f, center, radius, sample_size, eng);
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);  
-    std::cout << "L'integrale vale " << integral << std::endl;
-    std::cout << "In " << duration.count()  << " millisecondi" << std::endl;*/
-
-    std::vector<double> rv;
-    V<double> v0{std::vector<double>{1,1,1}};
-    V<double> v1{std::vector<double>{2,1,2}};
-    V<double> v2{std::vector<double>{1,2,4}};
-    V<V<double>> m{std::vector<V<double>>{v0,v1,v2}};
-    //for(int i = 0; i < m.size(); i++)
-    std::cout << det(m) << std::endl;
+    std::cout << "The result is " << integral << std::endl;
+    std::cout << "In " << duration.count()  << " milliseconds" << std::endl;
 
     return 0;
 }
