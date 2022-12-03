849d69f20a816b1fc38df3668749fd133de081eb
Maser-GitHub <simone000161@gmail.com>
2022-12-03 13:31:42 +0100

    Moved V to V.hpp

---
+++ V.hpp
@@ -0,0 +1,149 @@
+#ifndef V_HPP
+#define V_HPP
+
+#include <vector>
+
+//Template classes cannot be initialized outside of header
+
+template<typename T>
+class V{
+    public: 
+    std::vector<T> v;
+    
+    V<T>() {}
+
+    V<T>(std::vector<T> _v): v{_v} {}
+
+    V<T>(std::initializer_list<T> _v): v{_v} {}
+
+    V<T>(int size, T value){
+        v = std::vector<T>(size,value);
+    }
+
+    V<T>& operator += (const V<T>& rhs){
+        for(int i = 0; i< v.size(); i++){
+            this->v[i] += rhs.v[i];
+        }
+        return *this;
+    } 
+
+    V<T> operator + (V<T> const &r){
+        V<T> tmp = *this;
+        tmp+=r;
+        return tmp;
+    }
+
+    V<T>& operator += (T a){
+        for(int i = 0; i< v.size(); i++){
+            this->v[i] += a;
+        }
+        return *this;
+    } 
+
+    V<T> operator+ (T a){
+        V<T> tmp = *this;
+        tmp+=a;
+        return tmp;
+    }
+
+    V<T>& operator -= (const V<T>& rhs){
+        for(int i = 0; i< v.size(); i++){
+            this->v[i] -= rhs.v[i];
+        }
+        return *this;
+    } 
+
+    V<T> operator - (V<T> const &r){
+        V<T> tmp = *this;
+        tmp-=r;
+        return tmp;
+    }
+
+    V<T>& operator-= (T a){
+        for(int i = 0; i< v.size(); i++){
+            this->v[i] -= a;
+        }
+        return *this;
+    }
+
+    V<T> operator - (T a){
+        V<T> tmp = *this;
+        tmp-=a;
+        return tmp;
+    }
+
+    V<T> operator* (T a){
+        V<T> tmp = *this;
+        for(int i = 0; i< v.size(); i++){
+            tmp.v[i] *= a;
+        }
+        return tmp;
+    }
+
+    V<T> operator/ (T a){
+        V<T> tmp = *this;
+        for(int i = 0; i< v.size(); i++){
+            tmp.v[i] /= a;
+        }
+        return tmp;
+    }
+
+    T & operator[] (int i){
+        return v[i];
+    }
+
+    T operator[] (int i) const{
+        return v[i];
+    }
+
+    int size(){
+        return v.size();
+    }
+
+    auto begin(){
+        return v.begin();
+    }
+
+    auto end(){
+        return v.end();
+    }
+
+    template <class... Args>
+    void emplace_back(Args&&... args){
+        v.emplace_back(args ...);
+    }
+
+    void clear(){
+        v.clear();
+    }
+
+    void pop_back(){
+        v.pop_back();
+    }
+
+    void print(){
+        for(T el : v){
+            std::cout << " " << el;
+        }
+        std::cout << std::endl;
+    }
+
+    double sum(){
+        double sum = 0;
+        for(int i = 0; i < size(); i++){
+            sum += v[i];
+        }
+        return sum;
+    }
+
+    double norm(){
+        double norm = 0;
+        for(int i = 0; i < size(); i++){
+            norm += v[i] * v[i];
+        }
+        return norm;
+    }
+    // I might generalize the former 2 methods with "norm_p(int p)", but it is not useful
+};
+
+#endif
--- mc.cpp
+++ mc.cpp
@@ -4,144 +4,12 @@
 #include <array>
 #include <functional>
 #include <math.h>
+#include "V.hpp"
 
 // Everything is done with doubles for simplicity, but can be done with templates 
 
 // ================ VECTOR UTILS =======================
 
-// Vector class for when the data is conceptually a vector and not a collection
-template<typename T>
-class V{
-    public: 
-    std::vector<T> v;
-    V<T>() {}
-
-    V<T>(std::vector<T> _v): v{_v} {}
-
-    V<T>(std::initializer_list<T> _v): v{_v} {}
-
-    V<T>(int size, T value){
-        v = std::vector<T>(size,value);
-    }
-
-    V<T>& operator += (const V<T>& rhs){
-        for(int i = 0; i< v.size(); i++){
-            this->v[i] += rhs.v[i];
-        }
-        return *this;
-    } 
-
-    V<T> operator + (V<T> const &r){
-        V<T> tmp = *this;
-        tmp+=r;
-        return tmp;
-    }
-
-    V<T>& operator += (T a){
-        for(int i = 0; i< v.size(); i++){
-            this->v[i] += a;
-        }
-        return *this;
-    } 
-
-    V<T> operator+ (T a){
-        V<T> tmp = *this;
-        tmp+=a;
-        return tmp;
-    }
-
-    V<T>& operator -= (const V<T>& rhs){
-        for(int i = 0; i< v.size(); i++){
-            this->v[i] -= rhs.v[i];
-        }
-        return *this;
-    } 
-
-    V<T> operator - (V<T> const &r){
-        V<T> tmp = *this;
-        tmp-=r;
-        return tmp;
-    }
-
-    V<T>& operator-= (T a){
-        for(int i = 0; i< v.size(); i++){
-            this->v[i] -= a;
-        }
-        return *this;
-    }
-
-    V<T> operator - (T a){
-        V<T> tmp = *this;
-        tmp-=a;
-        return tmp;
-    }
-
-    V<T> operator* (T a){
-        V<T> tmp = *this;
-        for(int i = 0; i< v.size(); i++){
-            tmp.v[i] *= a;
-        }
-        return tmp;
-    }
-
-    V<T> operator/ (T a){
-        V<T> tmp = *this;
-        for(int i = 0; i< v.size(); i++){
-            tmp.v[i] /= a;
-        }
-        return tmp;
-    }
-
-    T & operator[] (int i){
-        return v[i];
-    }
-
-    T operator[] (int i) const{
-        return v[i];
-    }
-
-    int size(){
-        return v.size();
-    }
-
-    auto begin(){
-        return v.begin();
-    }
-
-    auto end(){
-        return v.end();
-    }
-
-    template <class... Args>
-    void emplace_back(Args&&... args){
-        v.emplace_back(args ...);
-    }
-
-    void clear(){
-        v.clear();
-    }
-
-    void pop_back(){
-        v.pop_back();
-    }
-
-    double sum(){
-        double sum = 0;
-        for(int i = 0; i < size(); i++){
-            sum += v[i];
-        }
-        return sum;
-    }
-
-    double norm(){
-        double norm = 0;
-        for(int i = 0; i < size(); i++){
-            norm += v[i] * v[i];
-        }
-        return norm;
-    }
-    // I might generalize the former 2 methods with "norm_p(int p)", but it is not useful
-};
 
 void print_vector(std::vector<double> x){
     for(auto el : x){
@@ -151,7 +19,7 @@ void print_vector(std::vector<double> x){
 }
 
 void print_vector(V<double> x){
-    for(auto el : x){
+    for(auto el : x.v){
         std::cout << " " << el;
     }
     std::cout << std::endl;
